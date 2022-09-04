629e11e66fa45c34b884be6ecf06c6ddf87e38ee
zafarivaev <z.ivaev@mail.ru>
2022-09-04 19:37:51 +0500

    Initial Commit

---
+++ TableInteraction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableInteraction
+//
+//  Created by Zafar Ivaev on 04/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
